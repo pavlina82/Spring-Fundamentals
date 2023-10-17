7fafa1d26e1124ee3d218a1eae6245ecd8a6eb65
Pavlina Petrova <114022470+pavlina82@users.noreply.github.com>
2023-10-17 20:56:28 +0300

    Delete mobilele directory

--- mobilele/mobilele/src/main/java/model/dto/UserRegistrationDTO.java
+++
@@ -1,8 +0,0 @@
-package model.dto;
-
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String email,
-                                  String password,
-                                  String confirmPassword) {
-}
--- mobilele/mobilele/src/main/java/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import model.dto.UserRegistrationDTO;
-
-public interface UserService {
-    void registerUser(UserRegistrationDTO userRegistrationDTO);
-}
